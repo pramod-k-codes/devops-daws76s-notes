5c65a754264ed8c29c151f68da596abd4f89e3a4
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-18 18:09:10 +0530

    Update 09-install-mysql.sh

--- shell/09-install-mysql.sh
+++ shell/09-install-mysql.sh
@@ -2,9 +2,27 @@
 # you need to use in brackets to execute properly
 ID=$(id -u)
 
-echo $EUID
+echo $EUID # Also returns the current user to determine its root or not
 echo $ID
 if [ "$ID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit 
+fi
+yum install mysql -y
+
+# validation of the installation
+
+if [ "$?" -ne 0 ]
+then 
+
+    echo "Installation failed"
+    exit 1
+
+fi  
+
+yum install firefox -y
+if [ "$?" -ne 0 ]
+then 
+  echo "Installation failed"
+  exit 1
 fi
