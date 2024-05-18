9fc9f3c68a812544a83864e6b507d551c670544f
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-18 17:02:58 +0530

    Update 09-install-mysql.sh

--- shell/09-install-mysql.sh
+++ shell/09-install-mysql.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+# you need to use in brackets to execute properly
+EUID=$(id -u)
 
-ID = id -u
-
-ID=$(id -u)
-
-
-echo ID
+echo $EUID
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
