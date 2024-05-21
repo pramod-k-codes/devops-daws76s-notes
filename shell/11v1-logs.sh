d167838d656b921288a5a9e0a01a0763982bd853
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-21 11:02:27 +0530

    Update 11v1-logs.sh

--- shell/11v1-logs.sh
+++ shell/11v1-logs.sh
@@ -37,11 +37,11 @@ VALIDATE_INSTALLATION(){
 }
 
 IS_ROOT_USER # this will fail in shell script because in shell you have to define the function before calling , calling should always at the end of file
-# INSTALL_APPLICATION "mysql"
-# INSTALL_APPLICATION "firefox"
-# INSTALL_APPLICATION "chrome"
-# INSTALL_APPLICATION "firewall-d"
-# INSTALL_APPLICATION "nginx"
+INSTALL_APPLICATION "mysql"
+INSTALL_APPLICATION "firefox"
+INSTALL_APPLICATION "chrome"
+INSTALL_APPLICATION "firewall-d"
+INSTALL_APPLICATION "nginx"
 
 
 
