728db46d44c8bdc9bc0cae978596c55e445f4028
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-22 08:48:39 +0530

    Update 13v1-install-packages.sh

--- shell/13v1-install-packages.sh
+++ shell/13v1-install-packages.sh
@@ -1,12 +1,20 @@
 #!bin/bash
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
+# Process text for formatting
+ECHO_PROCESS 
 
-
+ECHO_PROCESS(){
+    echo -e $1 $2 $N
+}
 
 ISROOTUSER(){
     if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
+    then echo $R"Please run as root"$N
         exit
     else
         echo "Running as root"
