27c85200c76fbad5f43020e739dff1f5e165d422
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-24 16:05:21 +0530

    Update 13v1-install-packages.sh

--- shell/13v1-install-packages.sh
+++ shell/13v1-install-packages.sh
@@ -31,14 +31,20 @@ IS_PACKAGE_INSTALLED(){
     fi
 }
 
-INSTALL_APPLICATION(){
-    ECHO_PROCESS $G "Installing application" $1
-    yum install $1 -y
-    if [ $? -eq 0 ]
-    then
-        ECHO_PROCESS $G "$1 installed"
+PERFORM_INSTALLATION(){
+    # Check if the package is available in the repositories
+    if yum list available "$package_name" > /dev/null 2>&1; then
+    ECHO_PROCESS $G "Package '$package_name' is valid."
+        ECHO_PROCESS $G "Installing application" $1
+        yum install $1 -y
+        if [ $? -eq 0 ]; then
+            ECHO_PROCESS $G "$1 installed"
+        else
+            ECHO_PROCESS $R "$1 installation failed"
+            exit 1
+        fi
     else
-        ECHO_PROCESS $R "$1 installation failed"
+        ECHO_PROCESS $R "Package '$package_name' is not available in the repositories enter correct package name."
         exit 1
     fi
 }
@@ -46,12 +52,16 @@ INSTALL_PACKAGES(){
     echo "Installing package no $#"
     if [ $# -gt 0 ] #    if [$# -gt 0] you need spaces
     then ECHO_PROCESS $G "Number of packages is $#, proceeding installation"
+    # this loop itereates over packages
         for current_package in $@
         do
+        # check if package is already installed
         IS_PACKAGE_INSTALLED $current_package 
+        # if package is not installed install it
             if [ $? -eq 0 ]
             then 
-                INSTALL_APPLICATION $current_package
+                PERFORM_INSTALLATION $current_package
+                # check if installation failed
                 if [ $? -ne 0 ]; then
                         ECHO_PROCESS $R "$current_package installation failed"
                 fi
@@ -68,3 +78,4 @@ INSTALL_PACKAGES ngnx gitt mysql # postfix net-tools
 # followng fixes needed
 #  tries to install installed packages only install packages which are not installed
 # print if package name is wrong
+  
