802d99b8055c726f99d48e499a768e78913d1705
PramodCodes <82228576+pramod-k-codes@users.noreply.github.com>
2024-05-26 09:05:18 +0530

    Update 13v1-install-packages.sh

--- shell/13v1-install-packages.sh
+++ shell/13v1-install-packages.sh
@@ -13,7 +13,7 @@ ECHO_PROCESS() {
 ISROOTUSER() {
     if [ "$EUID" -ne 0 ]; then
         ECHO_PROCESS $R"Please run as root"
-        exit
+        exit 1
     else
         ECHO_PROCESS $G "Running as root"
     fi
@@ -21,21 +21,21 @@ ISROOTUSER() {
 
 IS_VALID_PACKAGE() {
     echo "checking if package is valid"
-    if yum list available "$current_package" >/dev/null 2>&1; then
-        ECHO_PROCESS $G "$current_package is valid"
+    if yum list available "$1" >/dev/null 2>&1; then
+        ECHO_PROCESS $G "$1 is valid"
         return 0
     else
-        ECHO_PROCESS $R "$current_package is invalid"
+        ECHO_PROCESS $R "$1 is invalid"
         return 1
     fi
 }
 
 IS_PACKAGE_INSTALLED() {
     echo "checking if package is installed"
-    # if yum list installed $1 >/dev/null 2>&1; then
-    yum info $1
+    yum list installed $1 >/dev/null 2>&1
+    # yum info $1
     if [ $? -eq 0 ]; then
-        ECHO_PROCESS $R "$1 is installed , skipping"
+        ECHO_PROCESS $Y "$1 is installed , skipping"
         return 0
     else
         ECHO_PROCESS $G "$1 is not installed , proceeding with installing"
@@ -45,8 +45,16 @@ IS_PACKAGE_INSTALLED() {
 
 PERFORM_INSTALLATION() {
     # Check if the package is available in the repositories
-    if yum list available "$package_name" >/dev/null 2>&1; then
-        ECHO_PROCESS $G "Package '$package_name' is valid."
+    IS_VALID_PACKAGE $1
+    if [$? -ne 0 ]; then
+        # if $? value is zero then it means that the package is available so we don't need to do anything in this step
+        # but when package is unavailable break loop and getout so exit 1
+        ECHO_PROCESS $R "Package '$1' is not available in the repositories, enter correct package name."
+        exit 1
+    fi
+
+    IS_PACKAGE_INSTALLED $1
+    if [ $? -eq 0 ]; then
         ECHO_PROCESS $G "Installing application" $1
         yum install $1 -y
         if [ $? -eq 0 ]; then
@@ -56,12 +64,12 @@ PERFORM_INSTALLATION() {
             exit 1
         fi
     else
-        ECHO_PROCESS $R "Package '$package_name' is not available in the repositories enter correct package name."
-        exit 1
+        ECHO_PROCESS $R "Package '$1' is installed proceeding with next installations."
     fi
 }
 
 INSTALL_PACKAGES() {
+    ISROOTUSER
     ECHO_PROCESS $G "Installing package no $#"
     if [ $# -gt 0 ]; then #    if [$# -gt 0] you need spaces
         ECHO_PROCESS $G "Number of packages is $#, proceeding installation"
@@ -69,20 +77,8 @@ INSTALL_PACKAGES() {
         # Loop through each package
         for current_package in $@; do
 
-            IS_VALID_PACKAGE $current_package
-            # check if package is already installed
+            PERFORM_INSTALLATION $current_package
             IS_PACKAGE_INSTALLED $current_package
-            # if package is not installed install it
-            if [ $? -ne 0 ]; then # if 0 then package is installed already so check for non zero
-                PERFORM_INSTALLATION $current_package
-                # check if installation failed
-                IS_PACKAGE_INSTALLED $current_package
-                # if [ $? -ne 0 ]; then
-                #     ECHO_PROCESS $R "$current_package installation failed"
-                # fi
-            else
-                ECHO_PROCESS $R "$current_package is not installed"
-            fi
         done
     else
         ECHO_PROCESS $R "No packages to install"
@@ -90,7 +86,8 @@ INSTALL_PACKAGES() {
     fi
 }
 
-INSTALL_PACKAGES
+ISROOTUSER
+INSTALL_PACKAGES "$@"
 # followng fixes needed
 #  tries to install installed packages only install packages which are not installed
 # print if package name is wrong
