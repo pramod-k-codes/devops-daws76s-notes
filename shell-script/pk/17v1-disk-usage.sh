1009b60d5a5af4fe58c5ef4f606f99e60545d9e2
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-14 16:21:22 +0530

    disc usage

--- shell-script/pk/17v1-disk-usage.sh
+++ shell-script/pk/17v1-disk-usage.sh
@@ -2,7 +2,10 @@
 # set -xe
 DISK_LIST=$(df -hT | grep -vE 'tmp|File' | cut -d % -f1)
 DISC_NAME=$(echo $DISK_LIST | awk '{print$1}')
-DISK_USAGE=$(DISK_LIST | awk '{print$6}')
+ # DISC_NAME=$(DISK_LIST | awk '{print$1}') is wrong because you are not reading variable you need to echo to get output
+# DISC_NAME=$(echo DISK_LIST | awk '{print$1}') is wrong because you are not reading value of variable
+
+DISK_USAGE=$(echo $DISK_LIST | awk '{print$6}')
 echo -e "current disc data $DISK_LIST"
 echo -e "current disc name  $DISC_NAME"
 echo -e "current disc usage $DISK_USAGE"
