01453d4fb2cd5722d828304019f1049a6feb9a52
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-14 16:36:24 +0530

    disc usage

--- shell-script/pk/17v1-disk-usage.sh
+++ shell-script/pk/17v1-disk-usage.sh
@@ -9,14 +9,14 @@ DISK_USAGE=$(echo $DISK_LIST | awk '{print$6}')
 echo -e "current disc data $DISK_LIST"
 echo -e "current disc name  $DISC_NAME"
 echo -e "current disc usage $DISK_USAGE"
-while IFS=read line
+while IFS= read line
 do
 
   echo -e "$line -------------"
 
 
 
-done <<< $DISK_LIST
+done <<< "$DISK_LIST"
 
 
 
