8ac5a8fbf12f7dd85218b52dc7c43f950c986126
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-14 16:14:36 +0530

    Update 17v1-disk-usage.sh

--- shell-script/pk/17v1-disk-usage.sh
+++ shell-script/pk/17v1-disk-usage.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # set -xe
 DISK_LIST=$(df -hT | grep -vE 'tmp|File' | cut -d % -f1)
+DISC_NAME=$(DISK_LIST | awk '{print$1}')
+DISK_USAGE=$(DISK_LIST | awk '{print$6}')
+echo -e "current disc data $DISK_LIST"
+echo -e "current disc name  $DISC_NAME"
+echo -e "current disc usage $DISK_USAGE"
 
-echo -e "current disc usage data is $DISK_LIST"
+
+
