a280add5386d1bfbf8a706cb3a999b2ca90d21bc
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-14 19:11:57 +0530

    disc usage

--- shell-script/pk/17v1-disk-usage.sh
+++ shell-script/pk/17v1-disk-usage.sh
@@ -6,9 +6,9 @@ DISK_DATA=$(df -hT | grep -vE 'tmp|File' | cut -d % -f1)
 # DISC_NAME=$(echo DISK_DATA | awk '{print$1}') is wrong because you are not reading value of variable
 
 #DISK_USAGE=$(echo "$DISK_DATA" | awk '{print$6}')
-echo -e "current disc data $DISK_DATA"
-echo -e "current disc name  $DISC_NAME"
-echo -e "current disc usage $DISK_USAGE"
+#echo -e "current disc data $DISK_DATA"
+#echo -e "current disc name  $DISC_NAME"
+#echo -e "current disc usage $DISK_USAGE"
 #while IFS=read line # no spaces correct assignment such as ELEVEN="11" but this statement fails to read lines
 #while IFS = read line #This is syntax error because assignment should not have spaces around = in bash which makes assignment 'IFS = read line'
 #while IFS =read line #Also invalid statement because of space before assignment symbol = which makes assignment 'IFS ='
@@ -18,6 +18,9 @@ while IFS= read line ;do
    # DISC_NAME=$(DISK_DATA | awk '{print$1}') is wrong because you are not reading variable you need to echo to get output
    # DISC_NAME=$(echo DISK_DATA | awk '{print$1}') is wrong because you are not reading value of variable
   DISK_USAGE=$(echo "$DISK_DATA" | awk '{print$6F}')
+  echo -e "current disc data $DISK_DATA"
+  echo -e "current disc name  $DISC_NAME"
+  echo -e "current disc usage $DISK_USAGE"
   if [ $DISK_USAGE -ge 1 ]; then
       echo -e "## Disk usage of Disk $DISC_NAME is $DISK_USAGE which is greater than the threshold ##"
   fi
